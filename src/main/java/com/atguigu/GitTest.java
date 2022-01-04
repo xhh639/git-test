b95cd569b7427f7eb50c717e1091f3455c15cb61
xhh639 <84506444+xhh639@users.noreply.github.com>
2022-01-04 16:07:15 +0800

    pull commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("master test!");
         System.out.println("hello git!");
+        System.out.println("pull test!");
     }
 
 }
